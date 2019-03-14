d74b588935bee955213c958e5c76353f1cb0e2f2
Jordan Nique <jordan_nique@ens.univ-artois.fr>
2019-03-14 15:37:33 +0100

    Modification Spotify

--- src/environments/spotifyApiKey.ts
+++ src/environments/spotifyApiKey.ts
@@ -1,1 +1,1 @@
-export const SpotifyAPIKey = 'BQC7q37VzGJ-CbE2vcs12-EyRzvdxLilCjcH-r04HBJh50onxnv4ka1GLHnzLT-wuFjjcpTPJgmGMD0999M';
+export const SpotifyAPIKey = 'BQDFLWnAKn2a26zvw34Otbsz6umxhRUYLG_frFsMe0ICke2Nc0V6ios7rWDGOT0e9ixtGAOWeqYTMSTJTKI';
