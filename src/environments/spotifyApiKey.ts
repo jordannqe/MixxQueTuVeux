6acaef0bee3d960e6ac55a446ca0daddc0fa7068
Valentin Michel <valentin_michel@outlook.fr>
2019-03-27 21:15:05 +0100

    correctif

--- src/environments/spotifyApiKey.ts
+++ src/environments/spotifyApiKey.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const SpotifyAPIKey = 'BQAiRuEBynjkrNpK-vNWat8Zc3vcmWR4JuXyt3ofwI5UyHHkTIr4Io_Vn-3d2snLfTOocqrklR5kRUiky6Y';
-=======
-export const SpotifyAPIKey = 'BQBkZp9EcHlXjV4uENrxIAcgiy4XSESEtINi9i2P1SZZHguTYLKUGF8mzJBF2M-DmpHmolI66feLWuef2mE';
->>>>>>> 7e10fd58dc496a57ecbe259db9641ac37408cb0b
+export const SpotifyAPIKey = 'BQCNQww-nKej275DM2e8zQYI2VJ6PdMk8EYhRNMAqrkmlHUgo_t7QJsUBBdjxPxKtSJOeiO9wYpGPe-G7rI';
