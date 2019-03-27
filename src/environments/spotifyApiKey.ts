8217ea2ec338401d7772257ce6b72b6a9aabdb2b
Merveille Theo <theo_merveille@ens.univ-artois.fr>
2019-03-27 21:04:29 +0100

    pull

--- src/environments/spotifyApiKey.ts
+++ src/environments/spotifyApiKey.ts
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 export const SpotifyAPIKey = 'BQAiRuEBynjkrNpK-vNWat8Zc3vcmWR4JuXyt3ofwI5UyHHkTIr4Io_Vn-3d2snLfTOocqrklR5kRUiky6Y';
-=======
 export const SpotifyAPIKey = 'BQBkZp9EcHlXjV4uENrxIAcgiy4XSESEtINi9i2P1SZZHguTYLKUGF8mzJBF2M-DmpHmolI66feLWuef2mE';
->>>>>>> 7e10fd58dc496a57ecbe259db9641ac37408cb0b
